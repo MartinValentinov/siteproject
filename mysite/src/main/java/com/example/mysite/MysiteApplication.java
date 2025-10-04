9f01986c82abcefc256ab7af022351158b68476a
MartinValentinov <martin.k.valentinov.2022@elsys-bg.org>
2025-10-04 21:40:08 +0300

    idkbruh

--- mysite/src/main/java/com/example/mysite/MysiteApplication.java
+++ mysite/src/main/java/com/example/mysite/MysiteApplication.java
@@ -3,7 +3,7 @@ package com.example.mysite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class MysiteApplication {
     public static void main(String[] args) {
         SpringApplication.run(MysiteApplication.class, args);
---
+++ mysite/src/main/java/com/example/mysite/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package com.example.mysite;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .authorizeHttpRequests(registry->{
+                registry.requestMatchers("/home").permitAll();
+                registry.requestMatchers("/admin/**").hasRole("ADMIN");
+                registry.requestMatchers("/user/**").hasRole("USER");
+                registry.anyRequest().authenticated();
+            })
+            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+            .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User.builder()
+                .username("gc")
+                .password("$2a$12$Ccikw2IJwTsV2fbYa7Z0AuH1CgEY3esAtMtKrwhbUG500EZD4HW8q")
+                .roles("USER")
+                .build();
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password("$2a$12$J0sloTofHelXTmAL6mtn.OfZtH5FOvpP1ajpex.6Qa9AetnS5yt.a")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
